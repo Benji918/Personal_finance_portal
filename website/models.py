af7f7a1719de0243e475a615aa49e83f26f30212
Benjamin <kodiugos@gmail.com>
2022-12-26 03:33:52 -0800

    Added a Myapps model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class MyApp(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(default='', blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='my_apps')
+
+    def __str__(self):
+        return f'My app {self.id}, {self.name}'
+
+    class Meta:
+        verbose_name_plural = 'my apps'
+        ordering = ['name']
