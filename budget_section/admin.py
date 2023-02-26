4f90f6f14fcc6c841091cbaf9fb5fec310f4c967
Benjamin <kodiugos@gmail.com>
2023-02-26 14:25:41 -0800

    registered models to django admin

--- budget_section/admin.py
+++ budget_section/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, Transaction, BudgetTransaction, Budget
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Transaction)
+admin.site.register(BudgetTransaction)
+admin.site.register(Budget)
+
