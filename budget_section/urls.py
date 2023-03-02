7c7408baa89f6ba5f4deb1fca2bf2b62318754a3
Benjamin <kodiugos@gmail.com>
2023-03-02 02:05:13 -0800

    changes to budget views

--- budget_section/urls.py
+++ budget_section/urls.py
@@ -1,22 +1,25 @@
 from django.urls import path
 from budget_section import views
 from budget_section.views import CategoryCreateView, TransactionUpdateView, TransactionCreateView, CategoryUpdateView, \
-    BudgetCreateView, BudgetUpdateView
+    BudgetCreateView, BudgetUpdateView, BudgetListView, CategoryListView, BudgetDeleteView, \
+    CategoryDeleteView, TransactionListView
 
 app_name = 'budget_section'
 
 urlpatterns = [
-    # path('categories/', CategoryList.as_view(), name='category_list'),
+    path('categories/', CategoryListView.as_view(), name='category_list'),
     path('category/create/', CategoryCreateView.as_view(), name='category_create'),
-    path('category/<int:pk>/update/', CategoryUpdateView.as_view(), name='category_update'),
-    path('category/<int:pk>/delete/', views.delete_category, name='category_delete'),
-    # path('transactions/', TransactionList.as_view(), name='transaction_list'),
+    path('category/update/<int:pk>/', CategoryUpdateView.as_view(), name='category_update'),
+    path('category/delete/<int:pk>/', CategoryDeleteView.as_view(), name='category_delete'),
+
+    path('transactions/', TransactionListView.as_view(), name='transaction_list'),
     path('transaction/create/', TransactionCreateView.as_view(), name='transaction_create'),
-    path('transaction/<int:pk>/update/', TransactionUpdateView.as_view(), name='transaction_update'),
-    path('transaction/<int:pk>/delete/', views.delete_transaction, name='transaction_delete'),
-    # path('budgets', BudgetListView.as_view(), name='budget_list'),
+    path('transaction/update/<int:pk>/', TransactionUpdateView.as_view(), name='transaction_update'),
+    path('transaction/delete/<int:pk>/', TransactionDeleteView.as_view(), name='transaction_delete'),
+
+    path('budgets', BudgetListView.as_view(), name='budget_list'),
     path('budget/create/', BudgetCreateView.as_view(), name='budget_create'),
-    path('budget/<int:pk>/update/', BudgetUpdateView.as_view(), name='budget_update'),
-    path('budget/<int:pk>/delete/', views.delete_budget, name='budget_delete')
+    path('budget/update/<int:pk>/', BudgetUpdateView.as_view(), name='budget_update'),
+    path('budget/delete/<int:pk>/',  BudgetDeleteView.as_view(), name='budget_delete')
 
 ]
--- budget_section/views.py
+++ budget_section/views.py
@@ -1,215 +1,220 @@
+from datetime import datetime, timedelta
+
+from django.http import HttpResponseRedirect
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from django.db.models import Sum
+from django.db.models import Sum, Avg
 from django.views import View
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from .models import Category, Transaction, Budget
 from .forms import CategoryForm, TransactionForm, BudgetForm
 
+# BUDGET
 @method_decorator(login_required, name='dispatch')
-class ListView(View):
-    template_name = 'budget/home.html'
+class BudgetListView(ListView):
+    model = Budget
+    template_name = 'budget_section/category_transaction_budget_list.html'
+    paginate_by = 100
+    extra_context = {'list_what': 'Budget'}
 
-    def get(self, request):
-        categories = Category.objects.filter(user=request.user)
-        transactions = Transaction.objects.filter(user=request.user).order_by('-date')
-        budgets = Budget.objects.filter(user=request.user).order_by('-start_date')
+    def get_queryset(self):
+        user = self.request.user
+        # categories = Category.objects.filter(user=request.user)
+        # transactions = Transaction.objects.filter(user=request.user).order_by('-date')
+        return Budget.objects.filter(user=user).order_by('-start_date')
 
-        context = {
-            'categories': categories,
-            'transactions': transactions,
-            'budgets': budgets,
-        }
-        return render(request, self.template_name, context)
 
 @method_decorator(login_required, name='dispatch')
 class BudgetCreateView(View):
+    model = Budget
     form_class = BudgetForm
-    template_name = 'budget/form.html'
-    title = 'Add Budget'
+    template_name = 'budget_section/category_transaction_budget_form.html'
+    extra_context = {'header': 'Add budget'}
 
-    def get(self, request):
-        form = self.form_class()
-        context = {
-            'form': form,
-            'title': self.title,
-        }
-        return render(request, self.template_name, context)
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
-    def post(self, request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            budget = form.save(commit=False)
-            budget.user = request.user
-            budget.save()
-            messages.success(request, 'Budget has been created.')
-            return redirect('home')
-        context = {
-            'form': form,
-            'title': self.title,
-        }
-        return render(request, self.template_name, context)
+    def get_success_url(self):
+        messages.success(self.request, 'Budget created successfully!')
+        return reverse_lazy('budget_section:budget_list')
 
 
 @method_decorator(login_required, name='dispatch')
 class BudgetUpdateView(View):
+    model = Budget
     form_class = BudgetForm
-    template_name = 'budget/form.html'
-    title = 'Edit Budget'
+    template_name = 'budget_section/category_transaction_budget_list.html'
+    extra_context = {'header': 'Update Budget'}
 
-    def get(self, request, pk):
-        budget = get_object_or_404(Category, pk=pk, user=request.user)
-        form = self.form_class(instance=budget)
-        context = {
-            'form': form,
-            'title': self.title,
-        }
-        return render(request, self.template_name, context)
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
-    def post(self, request, pk):
-        budget = get_object_or_404(Budget, pk=pk, user=request.user)
-        form = self.form_class(request.POST, instance=budget)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Budget has been updated.')
-            return redirect('home')
-        context = {
-            'form': form,
-            'title': self.title,
-        }
-        return render(request, self.template_name, context)
+    def get_success_url(self):
+        messages.success(self.request, 'Budget updated successfully!')
+        return reverse_lazy('budget_section:budget_update')
 
-@login_required
-def delete_budget(request, pk):
-    budget = get_object_or_404(Budget, pk=pk, user=request.user)
-    budget.delete()
-    messages.success(request, 'budget has been deleted.')
-    return redirect('home')
+
+@method_decorator(login_required, name='dispatch')
+class BudgetDeleteView(DeleteView):
+    model = Budget
+    template_name = 'budget_section/category_transaction_budget_delete.html'
+    extra_context = {'delete_what': 'Budget'}
+
+    def get_queryset(self):
+        user = self.request.user
+        return Budget.objects.filter(user=user)
+
+    def get_success_url(self):
+        messages.success(self.request, 'Budget deleted successfully!')
+        return reverse_lazy('budget_section:budget_list')
+
+# CATEGORY
+@method_decorator(login_required, name='dispatch')
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'budget_section/category_transaction_budget_list.html'
+    paginate_by = 100
+    extra_context = {'list_what': 'Category'}
+
+    def get_queryset(self):
+        user = self.request.user
+        return Category.objects.filter(user=user).order_by('-id')
 
 @method_decorator(login_required, name='dispatch')
 class CategoryCreateView(View):
+    model = Category
     form_class = CategoryForm
-    template_name = 'budget/form.html'
-    title = 'Add Category'
+    template_name = 'budget_section/category_transaction_budget_form.html'
+    extra_context = {'header': 'Add Category'}
 
-    def get(self, request):
-        form = self.form_class()
-        context = {
-            'form': form,
-            'title': self.title,
-        }
-        return render(request, self.template_name, context)
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
-    def post(self, request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            category = form.save(commit=False)
-            category.user = request.user
-            category.save()
-            messages.success(request, 'Category has been created.')
-            return redirect('home')
-        context = {
-            'form': form,
-            'title': self.title,
-        }
-        return render(request, self.template_name, context)
+    def get_success_url(self):
+        messages.success(self.request, 'Category created successfully!')
+        return reverse_lazy('budget_section:category_list')
+
 
 @method_decorator(login_required, name='dispatch')
 class CategoryUpdateView(View):
+    model = Category
     form_class = CategoryForm
-    template_name = 'budget/form.html'
-    title = 'Edit Category'
+    template_name = 'budget_section/category_transaction_budget_form.html'
+    extra_context = {'header': 'Update Category'}
 
-    def get(self, request, pk):
-        category = get_object_or_404(Category, pk=pk, user=request.user)
-        form = self.form_class(instance=category)
-        context = {
-            'form': form,
-            'title': self.title,
-        }
-        return render(request, self.template_name, context)
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
-    def post(self, request, pk):
-        category = get_object_or_404(Category, pk=pk, user=request.user)
-        form = self.form_class(request.POST, instance=category)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Category has been updated.')
-            return redirect('home')
-        context = {
-            'form': form,
-            'title': self.title,
-        }
-        return render(request, self.template_name, context)
+    def get_success_url(self):
+        messages.success(self.request, 'Category created successfully!')
+        return reverse_lazy('budget_section:category_list')
 
-@login_required
-def delete_category(request, pk):
-    category = get_object_or_404(Category, pk=pk, user=request.user)
-    category.delete()
-    messages.success(request, 'Category has been deleted.')
-    return redirect('home')
+
+@method_decorator(login_required, name='dispatch')
+class CategoryDeleteView(DeleteView):
+    model = Budget
+    template_name = 'budget_section/category_transaction_budget_delete.html'
+    extra_context = {'delete_what': 'Category'}
+
+    def get_queryset(self):
+        user = self.request.user
+        return Category.objects.filter(user=user)
+
+    def get_success_url(self):
+        messages.success(self.request, 'Category deleted successfully!')
+        return reverse_lazy('budget_section:category_list')
+
+
+@method_decorator(login_required, name='dispatch')
+class TransactionListView(ListView):
+    model = Transaction
+    template_name = 'budget_section/category_transaction_budget_list.html'
+    context_object_name = 'transactions'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Get total amount spent in the last 7 days
+        today = datetime.date.today()
+        week_ago = today - timedelta(days=7)
+        context['total_spent_last_week'] = Transaction.objects.filter(date__range=[week_ago, today]).aggregate(
+            Sum('amount'))['amount__sum'] or 0
+
+        # Get average amount spent per category
+        context['average_spent_per_category'] = Transaction.objects.values('category__name').annotate(
+            average_spent=Avg('amount')).order_by('-date')[:5]
+
+        # Get total amount spent per category
+        context['total_spent_per_category'] = Transaction.objects.values('category__name').annotate(
+            total_spent=Sum('amount')).order_by('-date')[:5]
+
+        # Get the total categories
+        context['total_categories'] = Category.objects.count()
+
+        return context
+
 
 @method_decorator(login_required, name='dispatch')
 class TransactionCreateView(View):
+    model = Transaction
     form_class = TransactionForm
-    template_name = 'budget/form.html'
-    title = 'Add Transaction'
+    template_name = 'budget_section/category_transaction_budget_form.html'
+    extra_context = {'header': 'Add Transaction'}
 
-    def get(self, request):
-        form = self.form_class()
-        context = {
-            'form': form,
-            'title': self.title,
-        }
-        return render(request, self.template_name, context)
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
-    def post(self, request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            transaction = form.save(commit=False)
-            transaction.user = request.user
-            transaction.save()
-            messages.success(request, 'Transaction created Successfully!')
-            return redirect('home')
-        context = {
-            'form': form,
-            'title': self.title,
-        }
-        return render(request, self.template_name, context)
+    def get_success_url(self):
+        messages.success(self.request, 'Transaction created successfully!')
+        return reverse_lazy('budget_section:transaction_list')
 
 
 @method_decorator(login_required, name='dispatch')
 class TransactionUpdateView(View):
+    model = Transaction
     form_class = TransactionForm
-    template_name = 'budget/form.html'
-    title = 'Edit Category'
+    template_name = 'budget_section/category_transaction_budget_form.html'
+    extra_context = {'header': 'Add Transaction'}
 
-    def get(self, request, pk):
-        transaction = get_object_or_404(Transaction, pk=pk, user=request.user)
-        form = self.form_class(instance=transaction)
-        context = {
-            'form': form,
-            'title': self.title,
-        }
-        return render(request, self.template_name, context)
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
-    def post(self, request, pk):
-        transaction = get_object_or_404(Transaction, pk=pk, user=request.user)
-        form = self.form_class(request.POST, instance=transaction)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Transaction has been updated.')
-            return redirect('home')
-        context = {
-            'form': form,
-            'title': self.title,
-        }
-        return render(request, self.template_name, context)
+    def get_success_url(self):
+        messages.success(self.request, 'Transaction created successfully!')
+        return reverse_lazy('budget_section:transaction_list')
 
-@login_required
-def delete_transaction(request, pk):
-    transaction = get_object_or_404(Transaction, pk=pk, user=request.user)
-    transaction.delete()
-    messages.success(request, 'Transaction has been deleted.')
-    return redirect('home')
+
+@method_decorator(login_required, name='dispatch')
+class TransactionDeleteView(DeleteView):
+    model = Transaction
+    template_name = 'budget_section/category_transaction_budget_delete.html'
+    extra_context = {'delete_what': 'Transaction'}
+
+    def get_queryset(self):
+        user = self.request.user
+        return Transaction.objects.filter(user=user)
+
+    def get_success_url(self):
+        messages.success(self.request, 'Transaction deleted successfully!')
+        return reverse_lazy('budget_section:transaction_list')
