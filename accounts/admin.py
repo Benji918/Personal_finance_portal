3811b92e487953c6eb6ce89eacf50944cec784e5
USER <kodiugos@gmail.com>
2023-04-22 04:43:44 -0700

    File changes

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, CustomUser, SMSCode
+from django import forms
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
+
 
 # Register your models here.
-
-
 admin.site.register(Profile)
+admin.site.register(CustomUser)
+admin.site.register(SMSCode)
--- accounts/signals.py
+++ accounts/signals.py
@@ -1,19 +1,22 @@
 
 from django.db.models.signals import post_save
-from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import Profile
+from .models import Profile, CustomUser, SMSCode
 
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=CustomUser)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=CustomUser)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
 
 
+@receiver(post_save, sender=CustomUser)
+def post_save_generate_code(sender, instance, created, *args, **kwargs):
+    if created:
+        SMSCode.objects.create(user=instance)
 
