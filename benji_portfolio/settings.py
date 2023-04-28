6ba6dcfb4214a12a66e28486ec56956103485a91
USER <kodiugos@gmail.com>
2023-04-28 06:37:35 -0700

    file changes

--- benji_portfolio/settings.py
+++ benji_portfolio/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['personalfinaceportal-production.up.railway.app', 'http://127.0.0.1:9000/']
 
