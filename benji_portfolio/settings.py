fadfeb3af1fe1461bd8dca3a4e081ce9f7ebcdf6
USER <kodiugos@gmail.com>
2023-04-29 14:11:15 -0700

    File changes

--- benji_portfolio/settings.py
+++ benji_portfolio/settings.py
@@ -180,7 +180,7 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = BASE_DIR / 'media/profile_avatars'
+MEDIA_ROOT = BASE_DIR / 'media/profile_avatars/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
