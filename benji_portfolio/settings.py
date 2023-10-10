303d4695a50ff4d1601b41f718cc20bd39babad0
USER <kodiugos@gmail.com>
2023-10-10 01:53:02 +0100

    File changes

--- benji_portfolio/settings.py
+++ benji_portfolio/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['personalfinaceportal-production.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['personal-finance-portal.vercel.app', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
