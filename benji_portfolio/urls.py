7befe42e3c19cfc0c7085612e978a04be227eaa5
USER <kodiugos@gmail.com>
2023-10-10 02:34:53 +0100

    file changes

--- benji_portfolio/urls.py
+++ benji_portfolio/urls.py
@@ -27,15 +27,11 @@ urlpatterns = [
     path('savings_section/', include('savings_section.urls')),
 ]
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls)), ]
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
-    )
-    urlpatterns += static(
-        settings.STATIC_URL,
-        document_root=settings.MEDIA_ROOT,
-    )
+
--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,2 @@
-python manage.py collectstatic --no-input
-python manage.py migrate
+pip install -r requirements.txt
+python3.9 manage.py collectstatic
