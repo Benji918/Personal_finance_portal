4fbb6d68c1586720007f9ce75c5b6dcc926487f0
Benjamin <kodiugos@gmail.com>
2023-10-12 18:32:46 -0700

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
 # Build the project
-echo "Building the project..."
-pip install -r requirements.txt
-
-echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-
-echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+pip install -r requirements.txt 
+python3.9 manage.py collectstatic
